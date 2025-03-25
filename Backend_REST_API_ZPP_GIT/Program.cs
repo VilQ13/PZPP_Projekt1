da0a372ada8e3c2354c9056c15a2b7b714a3a960
Kostiantyn Hrekov <2004kostochka@gmail.com>
2025-03-25 01:40:37 +0100

    Dodany backend wraz z frontendem dla mechanizmu scrapowania grupy 1a kierunku informatyka

---
+++ Backend_REST_API_ZPP_GIT/Program.cs
@@ -0,0 +1,54 @@
+﻿using Backend_REST_API_ZPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_REST_API_ZPP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+         
+
+            // Add controllers
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy => policy.AllowAnyOrigin()
+                                   .AllowAnyMethod()
+                                   .AllowAnyHeader());
+            });
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add controllers for API
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson();  // Adds extended JSON support
+            builder.Services.AddHttpClient();
+
+
+            var app = builder.Build();
+
+            // Middleware setup
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+
+            // Serve static files from wwwroot
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("/home/index.html"); // Home page
+            });
+
+            app.Run();
+        }
+    }
+}
